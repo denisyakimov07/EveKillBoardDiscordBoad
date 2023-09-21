ed31fb9c8ed30303f43fd247d9c70d5d58b58625
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2023-09-21 18:15:13 -0400

    add loger

---
+++ Slash_commands/get_credentials.py
@@ -0,0 +1,8 @@
+import discord
+
+from discord_client import client
+
+
+@client.tree.command(name='Get admin credentials')
+async def get_admin_credentials(interaction: discord.Interaction):
+    await interaction.response.send_message('Test74879878987')

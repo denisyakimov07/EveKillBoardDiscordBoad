bc392bb086914daf3895a23d09331a3765c48973
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2023-09-21 18:36:40 -0400

    add loger

--- Slash_commands/get_credentials.py
+++ Slash_commands/get_credentials.py
@@ -1,7 +1,13 @@
 import discord
 
-from discord_client import client
+from discord_client import client, tree
 
-@client.tree.command(name='Get admin credentials')
+
+@tree.command(name='Eve bot')
 async def get_admin_credentials(interaction: discord.Interaction):
     await interaction.response.send_message('Test74879878987')
+
+
+@tree.command(name="commandname", description="My first application Command", guild=discord.Object(id=786028610653651004))
+async def first_command(interaction):
+    await interaction.response.send_message("Hello!")

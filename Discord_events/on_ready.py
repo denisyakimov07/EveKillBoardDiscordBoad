f3c4fa7d48efddc62a0fb0363a0f85af449a9e13
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2023-09-21 18:30:40 -0400

    add loger

--- Discord_events/on_ready.py
+++ Discord_events/on_ready.py
@@ -1,5 +1,7 @@
+import discord
+
 from DB.Creat_discord_server_settings import add_server_config
-from discord_client import client
+from discord_client import client, tree
 from loguru import logger
 
 
@@ -12,3 +14,5 @@ async def on_ready():
     servers = list(client.guilds)
     for server in servers:
         add_server_config(int(server.id))
+    await tree.sync(guild=discord.Object(id=786028610653651004))
+    print("ready")
--- discord_client.py
+++ discord_client.py
@@ -6,3 +6,4 @@ intents.members = True
 intents.guilds = True
 
 client = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(client)

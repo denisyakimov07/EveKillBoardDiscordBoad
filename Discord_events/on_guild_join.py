926588a5ad7b27999f0b2b17398c01a37222fe45
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2023-09-23 15:06:17 -0400

    add loger

--- Discord_events/on_guild_join.py
+++ Discord_events/on_guild_join.py
@@ -3,7 +3,7 @@ from discord_client import client
 from loguru import logger
 @client.event
 async def on_guild_join(guild):
-    if ADMIN_ROLE in [role_name.name for role_name in guild.fetch_roles()]:
+    if ADMIN_ROLE in [role_name.name for role_name in await guild.fetch_roles()]:
         logger.info(f"Role {ADMIN_ROLE} exist")
     else:
         await guild.create_role(name=ADMIN_ROLE)

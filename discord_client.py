461ef4f737fd384166728bec56786417195d249f
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2023-09-23 12:00:52 -0400

    add loger

--- discord_client.py
+++ discord_client.py
@@ -4,7 +4,6 @@ from discord.ext.commands import has_permissions
 
 client = commands.Bot()
 
-
 @client.slash_command(name="get_admin", guild_ids=[786028610653651004])
 @commands.has_any_role('user role', 'Moderators', 492212595072434186)
 async def get_credentials(ctx: discord.commands.context.ApplicationContext):

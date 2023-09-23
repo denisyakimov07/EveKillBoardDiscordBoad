dda5a3b964d7bade89eae797eb9280e0202706dc
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2023-09-23 13:45:37 -0400

    add loger

--- discord_client.py
+++ discord_client.py
@@ -5,7 +5,7 @@ from discord.ext.commands import has_permissions
 client = commands.Bot()
 
 @client.slash_command(name="get_admin", guild_ids=[786028610653651004])
-# @commands.has_any_role('user role', 'Moderators', 492212595072434186)
+@commands.has_any_role('Eve_Killboard')
 async def get_credentials(ctx: discord.commands.context.ApplicationContext):
     await ctx.user.send("You executed the slash command!")
 

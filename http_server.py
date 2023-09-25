f81a5cd15642a4284c3dcfd532561a2f0a5b2922
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2023-09-24 21:49:41 -0400

    add loger

--- http_server.py
+++ http_server.py
@@ -1,10 +1,13 @@
 from quart import Quart, request
+from quart_auth import basic_auth_required
 
 from Discord_requests.get_text_channels import get_text_channels
 from discord_client import client
-
+from environment import get_env
 
 app = Quart(__name__)
+
+
 
 @app.route('/webhook', methods=['POST'])
 async def myJSON():
@@ -18,5 +21,6 @@ async def myJSON():
         return 'Failed - Not a JSON', 400
 
 @app.route('/server_text_channels/<int:server_id>', methods=['GET'])
+@basic_auth_required(username_key=get_env().HTTP_USERNAME, password_key=get_env().HTTP_PASSWORD)
 async def server_text_channels(server_id):
     return f"{ await get_text_channels(server_id) }"
